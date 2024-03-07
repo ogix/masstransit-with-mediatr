359c475fdf8c1401dcd3c335353e74882fb1cad5
Olegas <olegas@warranty.expert>
2024-03-07 15:04:08 +0200

    consolidate CreateOrderCommandHandler

--- CreateOrderCommand.cs
+++ CreateOrderCommand.cs
@@ -9,26 +9,31 @@ public class CreateOrderCommand : IRequest
 
 public class CreateOrderCommandHandler : IRequestHandler<CreateOrderCommand>
 {
-    private readonly AppDbContext _dbContext;
+    private readonly AppDbContext _appDbContext;
     private readonly IPublishEndpoint _publishEndpoint;
 
-    public CreateOrderCommandHandler(AppDbContext dbContext, IPublishEndpoint publishEndpoint)
+    public CreateOrderCommandHandler(AppDbContext appDbContext, IPublishEndpoint publishEndpoint)
     {
-        _dbContext = dbContext;
+        _appDbContext = appDbContext;
         _publishEndpoint = publishEndpoint;
     }
 
     public async Task Handle(CreateOrderCommand request, CancellationToken cancellationToken)
     {
+        // this simulates transaction middleware
+        await using var transaction = await _appDbContext.Database.BeginTransactionAsync(cancellationToken);
+
         var order = new Order
         {
             Id = Guid.NewGuid()
         };
 
-        _dbContext.Orders.Add(order);
+        _appDbContext.Orders.Add(order);
 
+        // this also simulates publishing integration events from domain event handlers and then saving the changes to the database
         await _publishEndpoint.Publish(new OrderCreated(order.Id), cancellationToken);
+        await _appDbContext.SaveChangesAsync(cancellationToken);
 
-        await _dbContext.SaveChangesAsync(cancellationToken);
+        await transaction.CommitAsync(cancellationToken);
     }
 }
